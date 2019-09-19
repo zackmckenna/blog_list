b8822947d52653e44a35a23d77bde2ed40b49b56
Zachary Mckenna <zacharymckenna@Zacharys-MacBook-Pro.local>
2019-09-18 20:03:39 -0400

    Full blog API testing file

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -7,10 +7,43 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const body = request.body
 
-  await blog.save()
-  response.status(201).json(response)
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes === undefined ? 0 : body.likes,
+  })
+  if(blog.title && blog.url){
+    const savedBlog = await blog.save()
+    response.json(savedBlog.toJSON())
+  } else {
+    response.status(400).end()
+  }
 })
+
+blogsRouter.get('/:id', async (request, response, next) => {
+  try{
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+      response.json(blog.toJSON())
+    } else {
+      response.status(404).end()
+    }
+  } catch(exception) {
+    next(exception)
+  }
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try{
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch(exception) {
+    next(exception)
+  }
+})
+
 
 module.exports = blogsRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -9,23 +9,10 @@ const Blog = require('../models/blog')
 beforeEach(async () => {
   await Blog.deleteMany({})
 
-  let blogObject = new Blog(helper.initialBlogs[0])
-  await blogObject.save()
-
-  blogObject = new Blog(helper.initialBlogs[1])
-  await blogObject.save()
-
-  blogObject = new Blog(helper.initialBlogs[2])
-  await blogObject.save()
-
-  blogObject = new Blog(helper.initialBlogs[3])
-  await blogObject.save()
-
-  blogObject = new Blog(helper.initialBlogs[4])
-  await blogObject.save()
-
-  blogObject = new Blog(helper.initialBlogs[5])
-  await blogObject.save()
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
 })
 
 test('All blogs are returned', async () => {
@@ -49,34 +36,89 @@ test('All blogs returned in JSON format', async () => {
 test('Verify that blogs have id property.', async () => {
   let response = await api
     .get('/api/blogs')
-  console.log(response.body[0].id)
   expect(response.body[0].id).toBeDefined()
 })
 
 test('POST request adds a new blog post.', async () => {
   let blogToPost = new Blog(
     {
-      _id: "5a422bc61b54a676234d17f3",
       title: "New Blog",
       author: "Zack",
       url: "http://blog.newblog.com/uncle-bob/2016/05/01/TypeWars.html",
       likes: 2,
-      __v: 0
     }
   )
-  await blogToPost.save()
+  await api
+    .post('/api/blogs')
+    .send(blogToPost)
+    .expect(200)
+
 
   let response = await api
     .get('/api/blogs')
 
   expect(response.body.length).toBe(helper.initialBlogs.length + 1)
-  expect(response.body[response.body.length-1]).toStrictEqual({
-    id: "5a422bc61b54a676234d17f3",
-    title: "New Blog",
-    author: "Zack",
-    url: "http://blog.newblog.com/uncle-bob/2016/05/01/TypeWars.html",
-    likes: 2,
+  expect(response.body[response.body.length-1].author).toBe(
+    "Zack"
+  )
+})
+
+test('If there are no likes or if it is missing, it will set to a default of 0.', async () => {
+  let blogToPost = new Blog(
+    {
+      title: "New Blog",
+      author: "Zack",
+      url: "http://blog.newblog.com/uncle-bob/2016/05/01/TypeWars.html",
+    }
+  )
+  await api
+    .post('/api/blogs')
+    .send(blogToPost)
+    .expect(200)
+
+  let response = await api
+    .get('/api/blogs')
+  expect(response.body[response.body.length-1].likes).toBe(0)
+})
+
+test('If post request does not contain title and url properties, respond with 400', async () => {
+  let blogToPost = new Blog({
+    author: "zack"
   })
+
+  await api
+    .post('/api/blogs')
+    .send(blogToPost)
+    .expect(400)
+})
+
+test('Request blog by id', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+
+  const blogToFind = blogsAtStart[0]
+
+  const response = await api
+    .get(`/api/blogs/${blogToFind.id}`)
+    .expect(200)
+
+  expect(response.body).toEqual(blogToFind)
+})
+
+test('DELETE request deletes blog.', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+
+  const blogsAfterDelete = await helper.blogsInDb()
+
+  expect(blogsAtStart.length).toBe(blogsAfterDelete.length+1)
+
+  const titles = blogsAfterDelete.map(r => r.title)
+
+  expect(titles).not.toContain(blogToDelete.title)
 })
 
 afterAll(() => {
